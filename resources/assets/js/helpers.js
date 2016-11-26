7db7fe9a19a6f3a5ba8f305a53f55dd6a249eb1f
Simon Pioli <simon@simonpioli.com>
2016-11-26 19:11:48 +0000

    Starting full screen

--- resources/assets/js/helpers.js
+++ resources/assets/js/helpers.js
@@ -52,3 +52,25 @@ export function timeFormat(startString, endString) {
     }
     return start.format('HH:mm') + ' - ' + end.format('HH:mm');
 }
+
+export function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+export function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
