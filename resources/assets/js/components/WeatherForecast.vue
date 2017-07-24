9d606900ab56177615d799ebd69cdfde49d0331f
Simon Pioli <simon@simonpioli.com>
2017-07-24 19:50:18 +0100

    Update the styling, add some the wind direction component. Implement the last updated line

--- resources/assets/js/components/WeatherForecast.vue
+++ resources/assets/js/components/WeatherForecast.vue
@@ -1,6 +1,5 @@
 <!--
 TODO
-Basic styling for both sections
 Finish implementing the wind bearing
 Set background based on day vs night (Maybe find some animated backgrounds that match the icons)
 Implement time retrieved (maybe with relative time)
@@ -15,7 +14,11 @@ Implement time retrieved (maybe with relative time)
                 <p class="weather-current__detail weather-current__temp"><span class="detail__label">Temp: </span>{{ forecast.current.temperature.toFixed(0) }}ºC</p>
                 <!-- <p class="weather-current__detail weather-current__temp-feels-like" v-if="forecast.current.temperature != forecast.current.apparentTemperature"><span class="detail__label">Feels Like </span>{{ forecast.current.apparentTemperature.toFixed(0) }}ºC</p>  -->
                 <p class="weather-current__detail weather-current__temp-feels-like"><span class="detail__label">Feels Like </span>{{ forecast.current.apparentTemperature.toFixed(0) }}ºC</p> 
-                <p class="weather-current__detail weather-current__wind"><span class="detail__label">Wind: </span>{{ forecast.current.windSpeed.toFixed(0) }} MPH</p>
+                <p class="weather-current__detail weather-current__wind">
+                    <span class="detail__label">Wind: </span>
+                    {{ forecast.current.windSpeed.toFixed(0) }} MPH
+                    <windDirection :bearing="forecast.current.windBearing"></windDirection>
+                </p>
                 <p class="weather-current__detail weather-current__uv"><span class="detail__label">UV: </span>{{ forecast.current.uvIndex }}</p>
             </section>
             <section class="weather-hourly">
@@ -23,12 +26,20 @@ Implement time retrieved (maybe with relative time)
                     <li class="weather-item" v-for="(item, key) in forecast.hourly">
                         <span class="weather-item__time">{{ item.time }}</span>
                         <canvas class="weather-item__icon" :id="'icon-' +  key"></canvas>
-                        <span class="weather-item__summary">{{ item.summary }}</span>
-                        <span class="weather-item__temps">{{ item.temperature.toFixed(0) }}ºC <span class="weather-item__temps__fl" v-if="item.temperature != item.apparentTemperature">F/L {{ item.apparentTemperature.toFixed(0) }}ºC</span></span>
+                        <span class="weather-item__summary" v-html="item.summary"></span>
+                        <span class="weather-item__temps">
+                            {{ item.temperature.toFixed(0) }}ºC&nbsp;
+                            <span class="weather-item__temps__fl" v-if="item.temperature != item.apparentTemperature">F/L {{ item.apparentTemperature.toFixed(0) }}ºC</span>
+                            <span class="weather-item__temps__fl">F/L {{ item.apparentTemperature.toFixed(0) }}ºC</span>
+                        </span>
+                        <span class="weather-item__wind">
+                            {{ item.windSpeed.toFixed(0) }} MPH
+                            <windDirection :bearing="item.windBearing"></windDirection>
+                        </span>
                     </li>
                 </ul>
             </section>
-            <!-- <p class="weather-updated">{{ retrieved }}</p> -->
+             <p class="weather-updated">Last updated: {{ retrieved }}</p> 
         </div>
         <div class="weather-wrapper" v-else>
             <h1 class="weather-empty-header">No weather information available for {{ city }}</h1>
@@ -40,15 +51,17 @@ Implement time retrieved (maybe with relative time)
 import Echo from '../mixins/echo';
 
 import Grid from './Grid';
+import WindDirection from './weatherforecast/WindDirection';
 import { addClassModifiers } from '../helpers';
 import SaveState from '../mixins/save-state';
 var Skycons = require('../skycons');
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 export default {
 
     components: {
         Grid,
+        WindDirection
     },
 
     mixins: [Echo, SaveState],
@@ -62,7 +75,8 @@ export default {
     data() {
         return {
             forecast: [],
-            retrieved: 'Never'
+            retrieved: 'Never',
+            retrievedFormatted: 'Never'
         };
     },
 
@@ -72,10 +86,18 @@ export default {
 
     mounted() {
         this.refreshIcons();
+        this.refreshTime();
+        setInterval(this.refreshTime, 1000);
     },
 
     methods: {
         addClassModifiers,
+
+        refreshTime() {
+            if (this.retrieved !== 'Never') {
+                this.retrievedFormatted = this.retrieved.tz('Europe/London').fromNow();
+            }
+        },
 
         refreshIcons() {
             global.skycons = new Skycons.Skycons({"color": "white"});
---
+++ resources/assets/js/components/weatherforecast/WindDirection.vue
@@ -0,0 +1,8 @@
+<template>
+  <span class="wind-direction" :style="'transform: rotate(' + bearing + ');'">↑</span>
+</template>
+<script>
+export default {
+    props: ['bearing']
+}
+</script>
